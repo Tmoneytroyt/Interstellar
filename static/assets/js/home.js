232f638d937c61e9083c822809b3de39e2c4e08e
Tmoneytroyt <163927622+Tmoneytroyt@users.noreply.github.com>
2024-10-10 14:01:02 -0500

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -12,14 +12,16 @@ if (
   localStorage.getItem("ab") === "true"
 ) {
 
-      if (!popup || popup.closed) {
+  const popup = open("about:blank", "_blank");
+  if (!popup || popup.closed) {
     alert(
       "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
     );
-          if (!popup || popup.closed) {
-    alert(
-      "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
-    );
+  } else {
+    const doc = popup.document;
+    const iframe = doc.createElement("iframe");
+    const style = iframe.style;
+    const link = doc.createElement("link");
     const name = localStorage.getItem("name") || "My Drive - Google Drive";
     const icon =
       localStorage.getItem("icon") ||
